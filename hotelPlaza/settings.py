9ce9e9ee2d41c76890fcf8f55ad89fe832224613
Sanjana Ayshi <sanjana.ayshi92@gmail.com>
2024-01-17 23:48:30 +0600

    host settings

--- hotelPlaza/settings.py
+++ hotelPlaza/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2#icd2^ni(xqj(xo#8bjaxjdysdo!ep0sl6lk@ckg$6n%+#5oj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
